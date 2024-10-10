bc5d30c92e1509fe0f3b9bbfe5b90ee457b5856d
lgwarda <kukikarma@gmail.com>
2024-10-10 15:47:52 +0200

    Draw coordinate axes

--- sketch.js
+++ sketch.js
@@ -1,1 +1,6 @@
-function setup() { createCanvas(600, 400); } function draw() { background(255); }
+function draw() {
+    background(255);
+    stroke(0);
+    line(0, height / 2, width, height / 2); // X axis
+    line(width / 2, 0, width / 2, height); // Y axis
+  }
